3188b121e202f51626b8318dce9bd260087eea63
abkgami <yahabubakar1@gmail.com>
2025-01-05 17:44:50 +0100

    Add Navbar and Footer components; update layout and page title

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
    <div>
-    <h1 className="text-red-500">My name is ABk</h1>
+    <h1 className="text-red-500 font-bold text-2xl">My name is ABk</h1>
    </div>
   );
 }
